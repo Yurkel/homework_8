bb231daf9c8034b80fb76bba4fe7b3604c04dae4
Yuriy <yuriy-k@ukr.net>
2022-10-24 12:05:06 +0300

    all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,67 +10,32 @@ xhr.onload = function () {
   response.forEach((response) => renderItem(response));
 };
 
-const item = document.createElement("li");
+// version #1
 
 function renderItem({ name, photo, position }) {
-  const title = document.createElement("h1");
-  title.innerText = name;
-  const image = document.createElement("img");
-  image.src = photo;
-  const description = document.createElement("p");
-  description.innerText = position;
-  item.append(title, image, description);
+  const item = document.createElement("li");
+
+  (item.innerHTML = `
+  <h2>${name}</h2>
+  <img src="${photo}">
+  <p>${position}</p>
+  `),
+    list.append(item);
 }
 
-list.append(item);
-
-// function renderItem({ name, photo, position }) {
-//   const item = document.createElement("li");
-//   (document.li.innerHTML = <h2>${name}</h2>),
-//     (<img>${photo}</img>),
-//     (<p>${position}</p>);
-//   list.append(item);
-// }
-
-// function renderItem({ name, photo, position }) {
-//   const item = document.createElement("li");
-//   item.innerText = <h2>${name}</h2>;
-//   item.src = <img>${photo}</img>;
-//   item.innerText = <p>${position}</p>;
-//   list.append(item);
-// }
-
-// function renderPhoto(photo) {
-//   const item = document.createElement("img");
-//   item.src = photo;
-//   list.append(item);
-// }
-// function renderPosition(position) {
-//   const item = document.createElement("p");
-//   item.innerText = position;
-//   list.append(item);
-// }
+// version #2
 
 // xhr.onload = function () {
 //   const response = JSON.parse(xhr.response);
-//   response.forEach((response) => renderName(response.name));
-//   response.forEach((response) => renderPhoto(response.photo));
-//   response.forEach((response) => renderPosition(response.position));
+//   response.forEach(({ name, photo, position }) => {
+//     const item = document.createElement("li");
+//     const title = document.createElement("h2");
+//     const image = document.createElement("img");
+//     const description = document.createElement("p");
+//     title.innerText = name;
+//     image.src = photo;
+//     description.innerText = position;
+//     item.append(title, image, description);
+//     list.append(item);
+//   });
 // };
-
-// function renderName(name) {
-//   const item = document.createElement("h1");
-//   item.innerText = name;
-
-//   list.append(item);
-// }
-// function renderPhoto(photo) {
-//   const item = document.createElement("img");
-//   item.src = photo;
-//   list.append(item);
-// }
-// function renderPosition(position) {
-//   const item = document.createElement("p");
-//   item.innerText = position;
-//   list.append(item);
-// }
